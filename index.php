32ea13bf5a303e2881e4727947d1a9db13b161fa
wolfdeve <131177315+wolfdeve@users.noreply.github.com>
2023-04-18 17:24:59 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,202 @@
+<?php 
+
+require_once("inc/header.php");
+
+?>
+
+<html>
+	<head>
+		<title><?php echo name; ?></title>
+		<link href="./css/bootstrap.min.css" rel="stylesheet">
+		<link href="./css/theme.css?<?php echo time(); ?>" rel="stylesheet">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  		<script src="https://kit.fontawesome.com/de96098ae9.js" crossorigin="anonymous"></script>
+	</head>
+	<body class="bg-dark">
+		<div class="container mt-auto pb-5">
+  			<!-- Navigation Bar starts here -->
+  			<?php //require_once("inc/nav.php") ?>
+  			<!-- Navigation Bar ends here -->
+
+
+  			<!-- Content starts here -->
+  			<div class="row">
+  				<div class="col-md-8 mx-auto mt-5">
+  					<div class="card bg-dark card-shadow">
+  						<div class="card-header text-white bg-dark">
+  							Welcome to <?php echo name ?>
+  						</div>
+  						<div class="card-body bg-dark text-white">
+							Welcome to ETHOSEC! At ETHOSEC, we believe that ethics and security go hand-in-hand. Our mission is to promote responsible and secure practices within the technology industry. Here you will find resources, information, and insights on how to balance ethical considerations with the need for security. Thank you for visiting and we look forward to being your trusted partner in the ever-evolving landscape of ethics and security.
+  						</div>
+  					</div>
+  				</div>
+  				<div class="col-md-12 mt-5 text-white text-center">
+  					<h1 class="h1">Consulting</h1>
+  				</div>
+  				<div class="col-md-6 mt-5">
+  					<div class="card bg-dark card-shadow h-100">
+  						<div class="card-header text-white bg-dark">
+  							Jailbreaks / Prompt Engineering
+  						</div>
+  						<div class="card-body bg-dark text-white text-center">
+							<p>What to expect:</p>
+							<p>
+							Ask me anything. Develop your <b class="text-danger">Prompt-Engineering</b> skills with ChatGPT + JailBreak. Answers questions related to flagging, blocking, illegal content, and potential consequences of your actions with ChatGPT. I can help you get answers for the things you want answers to, but don't know how to get yourself, and how to get the best reply. Bonus: Through consulting, you get ChatGPT-4 prompt access via me for the duration of the session! This means you can prompt GPT-4 whatever you want without risking your own account, and additionally, I can help you prompt GPT-4 for better results!
+							</p>
+							<b class="text-success">
+								$40 / 30min
+							</b>
+  						</div>
+						<div class="card-footer text-center">
+							<small class="text-muted">
+								Before the paid consulting starts, you must be concise when telling me what you want and need. Tell me what you expect and want from the session, and I will develop a framework around that information. The paid session does not start before the framework is set.
+							</small>
+						</div>
+  					</div>
+  				</div>
+  				<div class="col-md-6 mt-5">
+  					<div class="card bg-dark card-shadow h-100">
+  						<div class="card-header text-white bg-dark">
+  							Programming
+  						</div>
+  						<div class="card-body bg-dark text-white text-center">
+							<p>What to expect:</p>
+							<p>
+							Ask me anything. Develop your <b class="text-danger">Programming</b> skills in Python or PHP. In this course I will help you create your own desired application by showing you how to navigate around a Programming language and how to improve your Logical and Problem Solving skills. In this course you may ask me anything Ethical, Unethical, Legal or Illegal. I will be more then happy to answer to your question and take the time to help you reach your goal.
+							Through consuliting you will gain access to all kinds of APIs and Tools your problem may require.
+							</p>
+							<b class="text-success">
+								$40 / hour
+							</b>
+  						</div>
+						<div class="card-footer text-center">
+							<small class="text-muted">
+								Before the paid consulting starts, you must be concise when telling me what you want and need. Tell me what you expect and want from the session, and I will develop a framework around that information. The paid session does not start before the framework is set.
+							</small>
+						</div>
+  					</div>
+  				</div>
+  				<div class="col-md-12 mt-5 text-white text-center">
+  					<h1 class="h1">Jailbreaks and Prompts</h1>
+  				</div>
+  				<div class="col-md-6 mt-5">
+  					<div class="card bg-dark card-shadow h-100">
+  						<div class="card-header text-white bg-dark">
+  							Jailbreaks
+  						</div>
+  						<div class="card-body bg-dark text-white text-center">
+							<p>What to expect:</p>
+							<p>
+								You will receive full access to a limited time Jailbreak for either <b class="text-info">GPT 3.5</b> or <b class="text-info">GPT 4</b>.
+								Using said Jailbreak will allow you to unlock the full potential of a limitless ChatGPT experience.
+								You will be able todo everything and anything that normal ChatGPT can not do.
+							</p>
+							<ul style="list-style: none;">
+								<li>GPT 3.5 JailBreak - <b class="text-success">$25</b></li>
+								<li>GPT 4.0 JailBreak - <b class="text-success">$40</b></li>
+							</ul>
+  						</div>
+						<div class="card-footer text-center">
+							<small class="text-muted">
+								Please note that a Jailbreak is not forever and it will eventually be patched by OpenAI Team.
+							</small>
+						</div>
+  					</div>
+  				</div>
+  				<div class="col-md-6 mt-5">
+  					<div class="card bg-dark card-shadow h-100">
+  						<div class="card-header text-white bg-dark">
+  							Prompt Listing
+  						</div>
+  						<div class="card-body bg-dark text-white text-center">
+							<p>What to expect:</p>
+							<p>
+								Using my JailBreaks, I can help you get answers that regular ChatGPT cannot, such as Additionally, I offer legal consulting services in areas such as:
+
+								Fundraising consultant, Product Launching, B2B Sales (Consulting, reviewing…), Customer Support, Product Management, UX Research requests, Copywriter requests, Social Media ideas,
+							</p>
+							<b class="text-success">
+								$20
+							</b>
+  						</div>
+						<div class="card-footer text-center">
+							<small class="text-muted">
+								Please note that consulting and prompt listings differ from one an other and you may only receive help up to a certain point. For anything extra you might wanna take a look at our consulting services
+							</small>
+						</div>
+  					</div>
+  				</div>
+  				<div class="col-md-12 mt-5 text-white text-center">
+  					<h1 class="h1">Scripts and Bots</h1>
+  				</div>
+  				<div class="col-md-6 mt-5">
+  					<div class="card bg-dark card-shadow h-100">
+  						<div class="card-header text-white bg-dark">
+  							Scripts
+  						</div>
+  						<div class="card-body bg-dark text-white text-center">
+							<p>What to expect:</p>
+							<p>
+								Anything you might need for automating such as AI, APIs, Automated Tools for different kind of websites or applications.
+							</p>
+							<ul style="list-style: none;">
+								<li>Python Scripts <b class="text-success">$20 - $100+</b></li>
+								<li>Website Bug Fixing <b class="text-success">$10 - $100+</b></li>
+								<li>Application Bug Fixing <b class="text-success">$20 - $130+</b></li>
+								<li>API Implementation <b class="text-success">$40 - $150+</b></li>
+							</ul>
+  						</div>
+						<div class="card-footer text-center">
+							<small class="text-muted">
+								Please note that any time of Bug fixing or features for your application take a certain time period and have a deadline.
+							</small>
+						</div>
+  					</div>
+  				</div>
+  				<div class="col-md-6 mt-5">
+  					<div class="card bg-dark card-shadow h-100">
+  						<div class="card-header text-white bg-dark">
+  							Bots
+  						</div>
+  						<div class="card-body bg-dark text-white text-center">
+							<p>What to expect:</p>
+							<p>
+								Telegram Bots, Discord Bots, Whatsapp, Viber any type of Bot you may require our Team can do it. The Bot features may vary on what your goal is.
+							</p>
+							<ul style="list-style: none;">
+								<li>Telegram DDOS Bot <b class="text-success">$40</b></li>
+								<li>Discord DDOS Bot <b class="text-success">$35</b></li>
+								<li>Custom Discord Bot <b class="text-success">$40 - $130+</b></li>
+								<li>Custom Telegram Bot <b class="text-success">$40 - $130+</b></li>
+							</ul>
+  						</div>
+						<div class="card-footer text-center">
+							<small class="text-muted">
+								Please note that consulting and prompt listings differ from one an other and you may only receive help up to a certain point. For anything extra you might wanna take a look at our consulting services
+							</small>
+						</div>
+  					</div>
+  				</div>
+
+  			</div>
+  			<!-- Content Ends here -->
+		</div>
+		<script src="./js/bootstrap.bundle.min.js"></script>
+		<script src="./js/jquery.min.js"></script>
+
+		<script>
+		$(document).ready(function() {
+		  var currentPage = window.location.pathname;
+
+		  console.log(currentPage);
+
+		  $(".list-group-item-action").each(function() {
+		    if ("/" + $(this).attr("href") === currentPage) {
+		      $(this).addClass("active");
+		    }
+		  });
+		});
+		</script>
+	</body>
+</html>
